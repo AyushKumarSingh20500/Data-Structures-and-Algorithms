2a744f9db17b34cbf0855fa1b5953af45032e1c1
AyushKumarSingh20500 <ayush.kumar.singh.20500@gmail.com>
2024-01-08 16:00:00 +0530

    08th January 2024

--- CodingNinjas/Tree/tree_again.py
+++ CodingNinjas/Tree/tree_again.py
@@ -158,3 +158,164 @@ def nodeGreaterThanX(root,X):
     return count
 
 print(nodeGreaterThanX(a,5))
+
+def heightOfTree(root):
+    if root == None:
+        return 0
+    
+    left = heightOfTree(root.left)
+    right = heightOfTree(root.right)
+    
+    return 1 + max(left,right)
+
+print(heightOfTree(a))
+
+def numberOfLeafInBT(root):
+    if root == None:
+        return 0
+    
+    count = 0
+    if root.left == None and root.right == None:
+        count += 1
+    
+    count += numberOfLeafInBT(root.left)
+    count += numberOfLeafInBT(root.right)
+    
+    return count
+
+print(numberOfLeafInBT(a))
+
+def printNodesatDepthK(root,k):
+    if root == None:
+        return
+    
+    if k == 0:
+        print(root.value, end=" ")
+        
+    printNodesatDepthK(root.left,k-1)
+    printNodesatDepthK(root.right,k-1)
+
+printNodesatDepthK(a,2)
+
+print("")
+
+# def replaceNodewithDepth(root,k = 0):
+#     if root == None:
+#         return
+    
+#     root.value = k
+    
+#     replaceNodewithDepth(root.left, k+1)
+#     replaceNodewithDepth(root.right,k+1)
+    
+#     return root
+
+# replacedRoot = replaceNodewithDepth(a)
+
+# BFS(replacedRoot)
+
+# print("")
+
+# a = Node(1)
+# a.left = Node(2)
+# a.right = Node(3)
+# a.left.left = Node(4)
+# a.left.right = Node(5)
+
+def isNodePresent(root,x): #by me, without looking very good, very proud
+    if root == None:
+        return False
+    
+    if root.value == x:
+        return True
+    
+    left = isNodePresent(root.left,x)
+    right = isNodePresent(root.right,x)
+    
+    return left or right
+
+print(isNodePresent(a,10))
+
+def nodesWithoutSibling(root):
+    if root == None:
+        return
+    
+    if (root.left != None and root.right == None) or (root.left == None and root.right != None):
+        if root.left == None:
+            print(root.right.value)
+        elif root.right == None:
+            print(root.left.value)
+            
+    nodesWithoutSibling(root.left)
+    nodesWithoutSibling(root.right)
+
+nodesWithoutSibling(a)
+
+
+
+def removeLeafNodes(root):
+    if root == None:
+        return None
+    
+    if root.left == None and root.right == None:
+        return None
+        
+    root.left = removeLeafNodes(root.left)
+    root.right = removeLeafNodes(root.right)
+    
+    return root
+
+# removeLeafNodesRoot = removeLeafNodes(a)
+
+# preorder(removeLeafNodesRoot)
+BFS(a)
+
+print("Break")
+
+def mirrorBT(root): # by me, without looking, very good very proud
+    if root == None:
+        return
+    
+    mirrorBT(root.left)
+    mirrorBT(root.right)
+    
+    temp = root.left
+    root.left = root.right
+    root.right = temp
+
+# mirrorBT(a)
+
+# BFS(a)
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+d = Node(4)
+e = Node(5)
+f = Node(6)
+
+a.left = b
+a.right = c
+b.left = d
+d.left = f
+c.right = e
+
+def isBalanced(root):
+    if root == None:
+        return True
+    
+    left = heightOfTree(root.left)
+    right = heightOfTree(root.right)
+    
+    if abs(left - right) > 1:
+        return False
+    
+    leftBt = isBalanced(root.left)
+    rightBT = isBalanced(root.right)
+    
+    if leftBt and rightBT:
+        return True
+    else:
+        return False
+
+print(isBalanced(a))
